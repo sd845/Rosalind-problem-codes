d53bc75ca9d3a31930548df558cd502b26fd36dd
sd845 <60407795+sd845@users.noreply.github.com>
2021-03-03 22:07:54 -0500

    Create p6.py

---
+++ p6.py
@@ -0,0 +1,15 @@
+def hamming_distance(sequence1, sequence2):
+    """ function that calculates hamming distance between two DNA sequences"""
+    
+    hamming_score = 0
+    
+    for nt1,nt2 in zip(sequence1,sequence2):
+        if nt1 != nt2: hamming_score +=1
+            
+    return hamming_score
+
+with open('rosalind_hamm.txt', 'r') as f:
+    data = f.read()
+    data = data.split("\n")
+    result = hamming_distance(data[0], data[1])
+    print(result)
