6968ddca90f26a3e9d17d280d9499cd961f69d87
sd845 <60407795+sd845@users.noreply.github.com>
2021-03-03 21:20:08 -0500

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,10 @@
+def make_rna(string):
+    """ function to transcribe DNA into RNA"""
+    
+    string = string.replace('T', 'U')
+    return string
+
+with open('rosalind_rna.txt', 'r') as f:
+    data = f.read()
+    result = make_rna(data)
+    print(result)
